60196c8d79ea832dcdc6f83b043131febea5d4aa
giullia <giulllia_mrs@yahoo.com>
2021-05-11 23:25:03 -0300

    Exercícios de herança

---
+++ POO/Cachorro.java
@@ -0,0 +1,15 @@
+package POO;
+
+public class Cachorro extends Animais {
+	
+	public Cachorro(String nome, int idade) {
+		super (nome, idade);
+	}
+	void correr () {
+		System.out.println("Correu.");
+	}
+
+	void imprimir () {
+		System.out.println("O(A)" + this.mostrarNome() + "tem" + this.mostrarIdade() + "aninhos");
+}
+}
---
+++ POO/Preguica.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class Preguica extends Animais{
+
+	public Preguica(String nome, int idade) {
+		super(nome, idade);
+		
+	}
+	
+	void subirEmArvores () {
+		System.out.println("Subiu na árvore.");
+	}
+
+	void imprimir () {
+		System.out.println("O(A)" + this.mostrarNome() + "tem" + this.mostrarIdade() + "aninhos");
+	}
+}
